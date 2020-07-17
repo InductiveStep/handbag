c5209431023e8fbe46faa7b653a8d564c597da41
InductiveStep <almost@gmail.com>
2020-07-17 17:36:26 +0100

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage("Welcome to handbag!")
-  packageStartupMessage("Follow @InductiveStep on twitter to improve my Kardashian Index")
+  packageStartupMessage("Follow @InductiveStep on Twitter to improve my Kardashian Index")
 }
